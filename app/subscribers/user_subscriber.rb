64375fb9a75256c96aea14a0a3e4ca82dd5232bf
Võ Anh Duy <voanhduy1512@live.com>
2014-04-08 23:27:17 +0700

    oiansoi

--- app/subscribers/user_subscriber.rb
+++ app/subscribers/user_subscriber.rb
@@ -1,5 +1,15 @@
+# 1
 class UserSubscriber < Subscriber
   def user_create user
     # Send mail code
   end
 end
+
+#2
+# class UserSubscriber
+#   include Subscriber
+#   subscriber :user_created, :user_create
+#   def user_create user
+#     # Send mail code
+#   end
+# end
